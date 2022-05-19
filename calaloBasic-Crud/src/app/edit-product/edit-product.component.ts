df78324a38043046e46954b97b800886e736dbf3
UBSIT_CalaloMichaelJoshua <20191160@s.ubaguio.edu>
2022-05-19 14:04:07 +0800

    FINAL PROJECT

--- calaloBasic-Crud/src/app/edit-product/edit-product.component.ts
+++ calaloBasic-Crud/src/app/edit-product/edit-product.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product} from '../product';
+import { PRODUCTS } from '../product-listings';
+import { ProductService } from '../product.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  @Input()product?:Product;
+
+  selectedProduct?: Product; 
+  
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    /* const id = this.route.snapshot.paramMap.get('id');
+    this.selectedProduct = PRODUCTS.find(selectedProduct => selectedProduct.id === id); */
+    this.getProduct();
   }
+  
+  getProduct(): void{
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProduct(id)
+    .subscribe(product => this.product=product);
+  }
+
+  goBack(): void{
+    this.location.back();
+  }
+
+  save(): void{
+    if(this.product){
+      this.productService.updateProduct(this.product)
+      .subscribe(()=>this.goBack());
+    }
+  }
+  
 
 }
--- calaloBasic-Crud/src/app/my-products/my-products.component.ts
+++ calaloBasic-Crud/src/app/my-products/my-products.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { PRODUCTS } from '../product-listings';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-my-products',
@@ -6,10 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-products.component.css']
 })
 export class MyProductsComponent implements OnInit {
+  products: Product[]=[];
+  
+  selectProduct?: Product;
+ /*    onSelect(product: Product): void{
+    this.selectProduct = product;
+    } */
 
-  constructor() { }
+  constructor(
+    //provider to access url parameter
+    private route:ActivatedRoute,
+    //productService
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    /* this.products = PRODUCTS; */
+    //call getProducts
+    this.getProducts();
+  }
+  getProducts(): void{
+    this.productService.getProducts().
+    subscribe(products => this.products = products);
+  
   }
 
+  add(pname:string):void{
+    pname=pname.trim();
+    if(!pname){return;}
+    this.productService.addProduct({pname} as Product)
+    .subscribe(product => {
+      this.products.push(product);
+    });
+  }
+
+  delete(product:Product): void{
+    this.confirmDelete();
+    this.products = this.products.filter(p => p !== product);
+    this.productService.deleteProduct(product.id)
+    .subscribe();
+  }
+
+  confirmDelete(){
+    alert('Are you sure you want to delete this account?');
+  }
 }
